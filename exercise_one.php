5b7504d127c7edf477801a442b38b2064538cc21
Snapy Shinda <worldchanger@gmail.com>
2020-10-17 19:46:35 -0700

    This is my first commit

---
+++ exercise_one.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Allado, Mark Joshua P."
+?>
---
+++ exercise_two.php
@@ -0,0 +1,76 @@
+<?php 
+$result = "";
+class calculator
+{
+    var $a;
+    var $b;
+
+    function checkopration($oprator)
+    {
+        switch($oprator)
+        {
+            case '+':
+            return $this->a + $this->b;
+            break;
+
+            case '-':
+            return $this->a - $this->b;
+            break;
+
+            case '*':
+            return $this->a * $this->b;
+            break;
+
+            case '/':
+            return $this->a / $this->b;
+            break;
+
+            default:
+            return "Sorry No command found";
+        }   
+    }
+    function getresult($a, $b, $c)
+    {
+        $this->a = $a;
+        $this->b = $b;
+        return $this->checkopration($c);
+    }
+}
+
+$cal = new calculator();
+if(isset($_POST['submit']))
+{   
+    $result = $cal->getresult($_POST['n1'],$_POST['n2'],$_POST['op']);
+}
+?>
+
+<form method="post">
+    <tr>
+        <td><strong><?php echo $result; ?><strong></td>
+    </tr>
+    <tr>
+        <br><td>Enter 1st Number</td>
+        <td><input type="text" name="n1"></td><br>
+    </tr>
+
+    <tr>
+        <td>Enter 2nd Number</td>
+        <td><input type="text" name="n2"></td><br>
+    </tr>
+
+    <tr>
+        <td>Select Oprator</td>
+        <td><select name="op">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select></td>
+    </tr><br>
+
+    <tr>
+        <td></td>
+        <td><input type="submit" name="submit" value=" = "></td>
+    </tr>
+
+</form>
