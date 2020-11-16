4d8ab425676f748a5958aa4d379cf6bd37a87b81
Mark-joshua <72646745+Mark-joshua@users.noreply.github.com>
2020-11-16 06:26:35 -0800

    second commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+class User
+{
+    public $name;
+    public $username;
+    public $email;
+
+    function friend($name)
+    {
+        echo "$name" , " is a friend";
+    }
+    function status($name)
+    {
+        echo "$name" , " has a new status";
+    }
+}
+
+$example = new User();
+echo $example -> friend("Mark Joshua") . "<br>";
+echo $example -> status("Mark Joshua");
+
+?>
